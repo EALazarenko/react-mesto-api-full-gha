54d9791ab9a5a436381a43b51d05ef690a453549
Ekaterina Lazarenko <lazarenkoea@yandex.ru>
2023-05-29 00:52:10 +0300

    auth err

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -10,6 +10,7 @@ export const checkToken = (token) => {
       "Content-Type": "application/json",
       "Authorization": `Bearer ${token}`
     },
+    credentials: 'include',
   })
     .then(checkResponse)
 };
@@ -21,6 +22,7 @@ export const register = (email, password) => {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
+    credentials: 'include',
     body: JSON.stringify({ email, password })
   })
     .then(checkResponse)
@@ -34,6 +36,7 @@ export const authorize = (email, password) => {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
+    credentials: 'include',
     body: JSON.stringify({ email, password })
   })
     .then(checkResponse)
