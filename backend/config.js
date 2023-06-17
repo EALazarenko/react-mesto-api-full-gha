68936bb80050586b7e1183d2f6da90ef2c86051e
Ekaterina Lazarenko <lazarenkoea@yandex.ru>
2023-06-17 21:17:14 +0300

    change port

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 3001,
   BASE_PATH: process.env.BASE_PATH || 'https://localhost.ru',
   JWT_SECRET: process.env.JWT_SECRET || 'super-secret-code',
 };
